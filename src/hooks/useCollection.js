3f10ac4a111ddeed0392b46a087a7981c782dd75
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-24 16:39:30 -0700

    Sort transactions by creation time

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -2,17 +2,21 @@ import { useEffect, useRef, useState } from "react";
 
 import { db } from "../firebase/config";
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   const query = useRef(_query).current;
+  const orderBy = useRef(_orderBy).current;
 
   useEffect(() => {
     let ref = db.collection(collection);
 
     if (query) {
       ref = ref.where(...query);
+    }
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy);
     }
 
     const unsub = ref.onSnapshot(
@@ -30,7 +34,7 @@ export const useCollection = (collection, _query) => {
     );
 
     return () => unsub();
-  }, [collection, query]);
+  }, [collection, query, orderBy]);
 
   return { documents, error };
 };
