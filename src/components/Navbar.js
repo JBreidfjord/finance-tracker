efc73ef93312bdf9d3ded3b2321ecbb18917e792
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-24 14:57:36 -0700

    Conditionally hide Nav buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>deepPockets</li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
+        <li className={styles.title}>
+          <NavLink to="/">deepPockets</NavLink>
         </li>
-        <li>
-          <NavLink to="/signup">Signup</NavLink>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {user ? (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">Signup</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
